e7bc6735309045fbd35c65fe2b7e28e3ecc71374
Dru Mumba <drusademumba@gmail.com>
2024-03-19 10:03:09 +0300

    A method that calculates the fewest number of operations needed to result in exactly n H characters in the file.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """Minimum Operations"""
+
+
+def minOperations(n):
+    """Minimum Operations"""
+    if n <= 1:
+        return 0
+    for i in range(2, n):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
