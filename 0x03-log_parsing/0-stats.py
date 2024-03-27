1aec7b69bb9ab450a848bd2e7c44f391e8ab9d57
Dru Mumba <drusademumba@gmail.com>
2024-03-27 13:52:08 +0300

    0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Log parsing"""
+
+count = 0
+file_size = 0
+stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+              "403": 0, "404": 0, "405": 0, "500": 0}
+
+
+def compute_metrics(status_code, file_sz):
+    """Compute metrics"""
+    print(f"File size: {file_sz}")
+    for key, val in sorted(status_code.items()):
+        if val != 0:
+            print(f"{key}: {val}")
+
+
+try:
+    while True:
+        try:
+            line = input()
+            data = line.strip().split()
+
+            if not line:
+                break
+
+            if len(data) >= 7:
+                size = int(data[-1])
+                stat_code = data[-2]
+
+                if stat_code in stat_codes:
+                    stat_codes[stat_code] += 1
+
+                file_size += size
+                count += 1
+
+                if count == 10:
+                    compute_metrics(stat_codes, file_size)
+                    file_size = 0
+                    count = 0
+
+        except KeyboardInterrupt:
+            break
+finally:
+    compute_metrics(stat_codes, file_size)
