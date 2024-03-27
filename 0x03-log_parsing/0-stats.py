302bb36e4dc5adac89166847091fdea2192cff03
Dru Mumba <drusademumba@gmail.com>
2024-03-27 14:06:49 +0300

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -37,10 +37,6 @@ try:
                 compute_metrics(stat_codes, file_size)
                 file_size = 0
                 count = 0
-                status_codes = {
-                    "200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0
-                }
 
         except KeyboardInterrupt:
             break
